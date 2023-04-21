93a71af949b0d45331cf189acac0bf517b648c4d
Shaden Smith <Shaden.Smith@microsoft.com>
2023-04-21 05:46:28 -0700

    fmt

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,45 +1,42 @@
-
 pub mod day1 {
 
-use std::fs;
-use std::io::{BufRead,BufReader};
-use std::collections::BinaryHeap;
+    use std::collections::BinaryHeap;
+    use std::fs;
+    use std::io::{BufRead, BufReader};
 
+    pub fn sumtopk_elves(path: &str, k: i32) -> i32 {
+        let reader = BufReader::new(fs::File::open(path).unwrap());
 
-pub fn sumtopk_elves(path: &str, k: i32) -> i32 {
-    let reader = BufReader::new(fs::File::open(path).unwrap());
+        let mut most_cals = 0;
+        let mut curr_cals = 0;
 
-    let mut most_cals = 0;
-    let mut curr_cals = 0;
+        let mut top_calories = BinaryHeap::new();
 
-    let mut top_calories = BinaryHeap::new();
-
-    for line in reader.lines() {
-        match line.unwrap().parse::<i32>() {
-            Ok(calories) => { 
-                curr_cals += calories;
-            },
-
-            // Blank line, move to the next elf
-            Err(_e) => {
-                if curr_cals > most_cals {
-                    most_cals = curr_cals;
+        for line in reader.lines() {
+            match line.unwrap().parse::<i32>() {
+                Ok(calories) => {
+                    curr_cals += calories;
                 }
 
-                top_calories.push(curr_cals);
-                curr_cals = 0;
+                // Blank line, move to the next elf
+                Err(_e) => {
+                    if curr_cals > most_cals {
+                        most_cals = curr_cals;
+                    }
+
+                    top_calories.push(curr_cals);
+                    curr_cals = 0;
+                }
             }
         }
-    }
 
-    let mut topk = 0;
-    for _ in 0..k {
-        topk += top_calories.pop().unwrap();
+        let mut topk = 0;
+        for _ in 0..k {
+            topk += top_calories.pop().unwrap();
+        }
+        return topk;
     }
-    return topk
 }
-}
-
 
 fn main() {
     let fname: String = String::from("day1/input.txt");
