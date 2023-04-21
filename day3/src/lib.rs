960afc5ccc0132c8818bfefa51454e440b6ac84c
Shaden Smith <Shaden.Smith@microsoft.com>
2023-04-21 08:30:57 -0700

    filling in day3

--- day3/src/lib.rs
+++ day3/src/lib.rs
@@ -1,14 +1,61 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::primitive::char;
+
+#[derive(Debug)]
+pub struct Rucksack {
+    pub left: String,
+    pub right: String,
+}
+
+impl Rucksack {
+    pub fn parse(leftright: &str) -> Rucksack {
+        let len = leftright.len();
+        if len % 2 != 0 {
+            panic!("leftright must be even length");
+        }
+
+        let half = len / 2;
+        Rucksack {
+            left: leftright[0..half].to_string(),
+            right: leftright[half..len].to_string(), 
+        }
+    }
+}
+
+pub fn parse_rucksacks(path: &str) -> Vec<Rucksack> {
+    let reader = BufReader::new(File::open(path).expect("File not found"));
+
+    let sacks: Vec<Rucksack> = reader.lines().map(|s| Rucksack::parse(&s.unwrap())).collect();
+    sacks
+}
+
+fn priority(token: char) -> u32 {
+    if !token.is_ascii_alphabetic() {
+        panic!("char must be [a-zA-Z]");
+    }
+
+    let (base_char, offset): (char, u32) = if token.is_lowercase() {
+        ('a', 1)
+    } else {
+        ('A', 27)
+    };
+
+    offset + u32::from(token) - u32::from(base_char)
 }
 
 #[cfg(test)]
-mod tests {
+mod tests_day3 {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_priority() {
+        assert_eq!(priority('a'), 1);
+        assert_eq!(priority('b'), 2);
+        assert_eq!(priority('z'), 26);
+        assert_eq!(priority('A'), 27);
+        assert_eq!(priority('Z'), 52);
     }
+
 }
---
+++ day3/tests/test_day3.rs
@@ -0,0 +1,19 @@
+
+#[cfg(test)]
+mod tests_day3{
+
+    use day3::*;
+
+    #[test]
+    fn test_rucksack_parse() {
+        let sack = Rucksack::parse("abcdEFGH");
+        assert_eq!(sack.left, "abcd");
+        assert_eq!(sack.right, "EFGH");
+    }
+
+    #[test]
+    fn test_pt1() {
+        let x = 3;
+        assert_eq!(x, 157);
+    }
+}
