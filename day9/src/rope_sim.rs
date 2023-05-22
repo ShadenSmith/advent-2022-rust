c0420491eaf51a044dab82474d7747f027f0ba1f
Shaden Smith <Shaden.Smith@microsoft.com>
2023-05-21 22:41:13 -0700

    parse commands

--- day9/src/rope_sim.rs
+++ day9/src/rope_sim.rs
@@ -1,4 +1,8 @@
-#[derive(PartialEq, Debug)]
+
+use std::fs::File;
+use std::io::{BufRead,BufReader};
+
+#[derive(PartialEq, Clone, Copy, Debug)]
 enum Move {
     Right,
     Left,
@@ -24,7 +28,27 @@ pub struct RopeSim {
 
 impl RopeSim {
     pub fn from_path(path: &str) -> Self {
-        RopeSim { cmds: vec![] }
+        let reader = BufReader::new(File::open(path).expect("File not found"));
+
+        let mut moves = vec![];
+
+        for line in reader.lines() {
+            // Ex: D 4
+            let line = line.unwrap();
+            let line: Vec<&str> = line.split(" ").collect();
+            let dir = Move::parse(line[0]);
+            let count = line[1].parse::<u64>().unwrap();
+
+            for _ in 0..count {
+                moves.push(dir);
+            }
+        }
+
+        RopeSim { cmds: moves }
+    }
+
+    pub fn len(&self) -> usize {
+        self.cmds.len()
     }
 
     pub fn count_tail_positions(&self) -> u64 {
@@ -34,13 +58,21 @@ impl RopeSim {
 
 #[cfg(test)]
 mod tests {
+    use super::Move;
+    use super::RopeSim;
+
 
     #[test]
     fn test_parse_move() {
-        use super::Move;
         assert_eq!(Move::parse("R"), Move::Right);
         assert_eq!(Move::parse("L"), Move::Left);
         assert_eq!(Move::parse("U"), Move::Up);
         assert_eq!(Move::parse("D"), Move::Down);
+    }
+
+    #[test]
+    fn test_parse_sim() {
+        let sim = RopeSim::from_path("input_test.txt");
+        assert_eq!(sim.len(), 24);
     }
 }
