30e413f979cd71879d08cc7c786d9c4d8f9ea52f
Shaden Smith <Shaden.Smith@microsoft.com>
2023-05-21 22:28:01 -0700

    parse single move

--- day9/src/rope_sim.rs
+++ day9/src/rope_sim.rs
@@ -1,14 +1,20 @@
 #[derive(PartialEq, Debug)]
 enum Move {
-    Right(u64),
-    Left(u64),
-    Up(u64),
-    Down(u64),
+    Right,
+    Left,
+    Up,
+    Down,
 }
 
 impl Move {
     pub fn parse(s: &str) -> Self {
-        Move::Right(3)
+        match s {
+            "R" => Move::Right,
+            "L" => Move::Left,
+            "U" => Move::Up,
+            "D" => Move::Down,
+            _ => panic!("Invalid move char"),
+        }
     }
 }
 
@@ -32,9 +38,9 @@ mod tests {
     #[test]
     fn test_parse_move() {
         use super::Move;
-        assert_eq!(Move::parse("R 3"), Move::Right(3));
-        assert_eq!(Move::parse("L 2"), Move::Left(3));
-        assert_eq!(Move::parse("U 8"), Move::Up(8));
-        assert_eq!(Move::parse("D 1"), Move::Down(1));
+        assert_eq!(Move::parse("R"), Move::Right);
+        assert_eq!(Move::parse("L"), Move::Left);
+        assert_eq!(Move::parse("U"), Move::Up);
+        assert_eq!(Move::parse("D"), Move::Down);
     }
 }
