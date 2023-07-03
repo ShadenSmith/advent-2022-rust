7ac12a1e5b9a71b31e592a33014468ad380b4a0a
Shaden Smith <shadentsmith@gmail.com>
2023-07-02 20:45:23 -0700

    fix part1 input

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,7 +1,7 @@
 use day13::packets;
 
 pub fn main() {
-    let part1 = packets::num_misordered_packets("inputs/test.txt");
+    let part1 = packets::num_misordered_packets("inputs/main.txt");
 
     println!("Part 1: {part1}");
 }
